3ad2a0b5f2e314b5ba02e53d0cd7eff1dd2b3714
roberto.fernandez <roberto.fernandez@E110L27.opendeusto.es>
2020-10-06 11:43:16 +0200

    Clase 6/10

--- Progam3/Test/Pruebas/UtilsStringTest.java
+++ Progam3/Test/Pruebas/UtilsStringTest.java
@@ -8,7 +8,11 @@ public class UtilsStringTest {
 
 	@Test
 	public void testquitarTabsYSaltosLinea() {
-		fail("Not yet implemented");
+		String prueba = "Hola\nEsto es un string con tres líneas\ny\tvarios\ttabuladores.";
+		assertEquals("Hol...", UtilsString.wrapString(prueba, 3));
+		assertEquals("Hola...", UtilsString.wrapString(prueba, 4));
+		assertEquals("Hola\nE...", UtilsString.wrapString(prueba, 6));
+		assertEquals("Hola\nEsto es un string con tres líneas\ny\tvarios\ttabuladores.", UtilsString.wrapString(prueba, 63));
 	}
 
 }

fa692380c75353c597d73a665b24604f73293cf7
robertofdzb10 <61292228+robertofdzb10@users.noreply.github.com>
2020-10-15 18:05:06 +0200

    JUnnit Pruebas Paramétricas 2.0

---
+++ Progam3/Test/Pruebas/TestFuncionParaTest.java
@@ -0,0 +1,37 @@
+package Pruebas;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+import Clase.FuncionParaTest;
+
+@RunWith(value = Parameterized.class)
+public class TestFuncionParaTest {
+
+	private int a, exp;
+	
+	@Parameters
+	public static Iterable<Object[]> getData(){
+		return Arrays.asList(new Object [][] {
+			{0,1} , {1,1} , {2,2} , {3,6} , {4,24} , {5,120} , {6,720} , {7,5040} , {8,	40320} , {9,362880} , {10,3628800} , {11,39916800} , {12,479001600} 
+		});
+	}
+	
+	public TestFuncionParaTest(int a, int exp) {
+		this.a =a;
+		this.exp = exp;
+	}
+	
+	@Test
+	public void testFactorial() {
+		FuncionParaTest fac = new FuncionParaTest();
+		int result = fac.factorial(a);
+		assertEquals(exp, result);
+	}
+}
